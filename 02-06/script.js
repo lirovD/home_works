c6b9f1e8625132db314f463cc6458830d0024ce7
FAinShop <fashop127@gmail.com>
2022-06-04 16:30:29 +0600

    init: start

---
+++ 02-06/script.js
@@ -0,0 +1,26 @@
+//  1
+const userSalaries = {
+    Ella: 45000,
+    Sophia: 35000,
+    Ellie: 37000,
+}
+
+delete userSalaries.Ella;
+
+alert(userSalaries);
+
+
+//  2
+let object = {
+    keyA: 5,
+    keyB: 6,
+    keyC: 10,
+}
+
+alert(object.keyA + object.keyB + object.keyC);
+
+
+//  3
+let number = data ? 3 : 5;
+
+alert(number);
