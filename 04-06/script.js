c6a3c7852c80c31059049491a21c2fd3f05f1bdd
FAinShop <fashop127@gmail.com>
2022-06-04 16:33:34 +0600

    init: start

---
+++ 04-06/script.js
@@ -0,0 +1,21 @@
+//  1
+const arr1 = ['a', 'b', 'c', 'd'];
+const result1 = (`${arr1[0]}+${arr1[1]}, ${arr1[2]}+${arr1[3]}`);
+
+console.log(result1);
+
+
+//  2
+const arr2 = [10, 15, 9, 10];
+const result2 = arr2[0] * arr2[1] + arr2[2] * arr2[3];
+
+console.log(result2);
+
+
+
+//  3
+const  arr3 = ['a', 'b', 'c'];
+arr3.push(1, 2, 3);
+
+console.log(arr3);
+
