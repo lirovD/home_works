b0d62715b9e1219cde78467b7d9004d7d48509e0
FAinShop <fashop127@gmail.com>
2022-06-16 16:31:06 +0600

    feat: added practice work and 2 task

---
+++ 16-06/script.js
@@ -0,0 +1,57 @@
+//     Practice work
+/*
+function fillArr(value, size) {
+    let arr = [];
+    for(let i = 0; i < size; i++){
+        arr.push(value);
+    }
+    return arr;
+}
+
+console.log(fillArr('Hello', 5));
+*/
+
+
+
+/*
+function calculateFinalGrade(grade,projects) {
+    if(grade > 90 || projects > 10) {
+        return 100;
+    } else if(grade > 75 && projects >= 5){
+        return 90;
+    } else if(grade > 50 && projects >= 2){
+        return 75;
+    } else if(grade > 30 && projects >= 1){
+        return 50;
+    } else{
+        return 0;
+    }
+
+}
+
+console.log(calculateFinalGrade(76,2));
+*/
+
+
+
+//     1
+/*
+let arr = [];
+
+for(let i = 0; i <= 9;i++) {
+    arr[i] = 'x';
+}
+console.log(arr);
+
+*/
+
+
+//     2
+/*
+let numbers = [1,2,115,4,58,5,7,99];
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 4) {
+        alert('ЕСТЬ!!!');
+        break;
+    }
+}*/
